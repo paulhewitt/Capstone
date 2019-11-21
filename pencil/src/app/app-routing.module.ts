aa6b80b34cbee074ce7c6657dfb87b3c949dcf3b
Paul Hewitt <thephewitt@gmail.com>
2019-11-21 17:17:09 -0600

    feat(app): added home, nav, and signup components; added routing

--- pencil/src/app/app-routing.module.ts
+++ pencil/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+
+{
+  path: '', component: HomeComponent,
+},
+{
+  path: 'signup', component: SignupComponent,
+},
+{
+  path: '**',
+  redirectTo: '',
+  pathMatch: 'full'
+}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    anchorScrolling: 'enabled',
+    scrollPositionRestoration: 'enabled',
+    onSameUrlNavigation: 'reload'
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
