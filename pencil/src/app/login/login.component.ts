bfb7998c37506a1baaf5a15066448f1af6977116
Paul Hewitt <thephewitt@gmail.com>
2020-01-27 21:16:40 -0600

    fix(userService): tidying up the user service

--- pencil/src/app/login/login.component.ts
+++ pencil/src/app/login/login.component.ts
@@ -8,30 +8,18 @@ import { UserService } from '../../services/user.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  private user: SocialUser;
-  private loggedIn: boolean;
+
 
   constructor(private authService: AuthService, private userService: UserService) { }
 
   ngOnInit() {
     this.authService.authState.subscribe((user) => {
-      this.user = user;
-      this.loggedIn = (user != null);
       this.userService.setUser(user);
       this.userService.setLoggedIn(user != null);
     });
   }
 
-  // signInWithFB(): void {
-  //   this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
-  // }
-
-  // signOut(): void {
-  //   this.authService.signOut();
-  //  }
-
   signIn() {
     this.userService.signIn();
   }
-
 }
