60c43ad30a0b486950bc3b17523832ed282d043b
Paul Hewitt <thephewitt@gmail.com>
2020-03-03 16:38:48 -0600

    fix(login): fixing google maps

--- pencil/src/app/login/login.component.ts
+++ pencil/src/app/login/login.component.ts
@@ -18,28 +18,11 @@ export class LoginComponent implements OnInit {
     this.authService.authState.subscribe((user) => {
       this.userService.setUser(user);
       this.userService.setLoggedIn(user != null);
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.displayLocationInfo);
+      }
     });
 
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(displayLocationInfo)
-    }
-    
-    function displayLocationInfo (position) {
-      var blockToInsert = document.createElement('iframe')
-      var containerBlock
-      const lng = position.coords.longitude
-      const lat = position.coords.latitude
-    
-      blockToInsert.style.width = '100%'
-      blockToInsert.style.height = '600px'
-      blockToInsert.style.border = '0'
-    
-      blockToInsert.src = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyBdffraumdcWacCqb2uot3eZ4DmizWRn8g&q=' + lat + ',' + lng
-      containerBlock = document.getElementById('map')
-      containerBlock.appendChild(blockToInsert)
-      console.log(`longitude: ${lng} | latitude: ${lat}`)
-      console.log(blockToInsert.src)
-    }
 
     this.homeService.getMockData().subscribe(
       (businesses) => {
@@ -54,5 +37,22 @@ export class LoginComponent implements OnInit {
   signIn() {
     this.userService.signIn();
   }
+
+  displayLocationInfo(position) {
+    const blockToInsert = document.createElement('iframe');
+    const containerBlock = document.getElementById('map');
+    const lng = position.coords.longitude;
+    const lat = position.coords.latitude;
+
+    blockToInsert.style.width = '100%';
+    blockToInsert.style.height = '600px';
+    blockToInsert.style.border = '0';
+
+    blockToInsert.src = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyBdffraumdcWacCqb2uot3eZ4DmizWRn8g&q=' + lat + ',' + lng;
+    containerBlock.appendChild(blockToInsert);
+    console.log(`longitude: ${lng} | latitude: ${lat}`);
+    console.log(blockToInsert.src);
+  }
+
 
 }
