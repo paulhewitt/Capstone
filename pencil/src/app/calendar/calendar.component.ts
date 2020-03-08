bd2ed6c10d8144461f56db5fd4ec6da4a3056c40
Ian Quach <quach.ian07@gmail.com>
2020-03-07 18:33:55 -0600

    feat(calendar): added calendar component with clickable events to create and dragable/editable times.

--- pencil/src/app/calendar/calendar.component.ts
+++ pencil/src/app/calendar/calendar.component.ts
@@ -1,8 +1,10 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
+import { FullCalendarComponent } from '@fullcalendar/angular';
 import { EventInput } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGrigPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+
 
 @Component({
   selector: 'app-calendar',
@@ -12,23 +14,38 @@ import interactionPlugin from '@fullcalendar/interaction';
 
 export class CalendarComponent implements OnInit {
 
+  @ViewChild('calendar', {static: false}) calendarComponent: FullCalendarComponent;
+  calendarVisible = true;
   calendarPlugins = [dayGridPlugin, timeGrigPlugin, interactionPlugin];
-  calendarWeekends = true;
+  calendarWeekends = false;
   calendarEvents: EventInput[] = [
-    {}
+    { title: 'Event Now', start: new Date() }
   ];
   constructor() { }
 
   ngOnInit() {
   }
 
+  addEvent(arg){
+    const titlePrompt = prompt('Who is this appointment for?');
+    this.calendarEvents = this.calendarEvents.concat({ // add new event data. must create new array
+      title: titlePrompt,
+      start: arg.date,
+      allDay: arg.allDay
+    });
+  }
+
   handleDateClick(arg) {
-    if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
-      this.calendarEvents = this.calendarEvents.concat({ // add new event data. must create new array
-        title: 'New Event',
-        start: arg.date,
-        allDay: arg.allDay
-      });
+    if (confirm('Would you like to add an appointment for ' + arg.dateStr + ' ?')) {
+      this.addEvent(arg);
     }
   }
+
+  // modifyTitle(eventIndex, newTitle) {
+  //   let calendarEvents = this.calendarEvents.slice(); // a clone
+  //   let singleEvent = Object.assign({}, calendarEvents[eventIndex]); // a clone
+  //   singleEvent.title = newTitle;
+  //   calendarEvents[eventIndex] = singleEvent;
+  //   this.calendarEvents = calendarEvents; // reassign the array
+  // }
 }
